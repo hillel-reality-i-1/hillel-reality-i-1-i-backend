efdbae75e05703f7a8d3659eed719076b73f9361
gelono <gelonooz@gmail.com>
2023-11-14 20:56:10 +0200

    Fix urls

--- core/urls.py
+++ core/urls.py
@@ -23,11 +23,7 @@ from django.urls import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("dj_rest_auth.urls")),
-    path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/", include("apps.users.urls")),  # тут находится ендпоинт с регистрацией .../api/v1/auth/registration/
     path("api/v1/content/", include("apps.content.api.urls")),
     path("api/v1/prof_service/", include("apps.expert.urls")),
-    #
-    # path("api/v1/", include(
-    #     #...
-    # )),
 ]
