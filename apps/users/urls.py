dd5b8cd2ed4bb8b284606ff0696806250570ac09
Giperelk <mizerisaid1722@gmail.com>
2023-11-15 19:54:12 +0200

    Valid Verify Email

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,10 @@
 from apps.users.views.custom_register_view import CustomRegisterView
-from allauth.account.views import ConfirmEmailView
-from django.urls import (
-    path,
-    include,
-)
-
+from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
+from django.urls import path, include
 
 urlpatterns = [
     path("auth/", include("dj_rest_auth.urls")),
     path("auth/registration/", CustomRegisterView.as_view(), name="rest_register"),
     path("accounts/confirm-email/<str:key>/", ConfirmEmailView.as_view(), name="account_confirm_email"),
+    path("accounts/verify-email", VerifyEmailView().as_view(), name="account_email_verification_sent")
 ]
