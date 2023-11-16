547be2fadda731b7d227547d41357d364b3c32e4
gelono <gelonooz@gmail.com>
2023-11-16 11:12:31 +0200

    Add generate_prof_and_serv.py

---
+++ apps/expert/management/commands/generate_prof_and_serv.py
@@ -0,0 +1,28 @@
+from django.core.management import BaseCommand
+import os
+import json
+import django
+
+from apps.expert.models import Profession, Service
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
+json_file_path = "professions_and_services.json"
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open(json_file_path, encoding="utf-8") as file:
+            data = json.load(file)
+
+        """
+            Load in db Professions and Services
+        """
+
+        for profession_name, services_list in data.items():
+            profession, created = Profession.objects.get_or_create(name=profession_name)
+
+            for service_name in services_list:
+                service, created = Service.objects.get_or_create(name=service_name)
+                service.professions.add(profession)
