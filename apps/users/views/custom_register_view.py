185887f481e4784c4e1c1d0fe08fc4ab1e497056
Giperelk <mizerisaid1722@gmail.com>
2023-11-30 14:42:01 +0200

    Update Registration View

--- apps/users/views/custom_register_view.py
+++ apps/users/views/custom_register_view.py
@@ -4,9 +4,18 @@ from dj_rest_auth.registration.views import RegisterView
 
 from apps.users.models import User
 from apps.users.serializers.custom_registration_serializer import CustomRegistrationSerializer
+from django.utils.translation import gettext_lazy as _
+from allauth.account import app_settings as allauth_account_settings
 
 
 class CustomRegisterView(RegisterView):
     serializer_class = CustomRegistrationSerializer
     permission_classes = [AllowAny]
     queryset = User.objects.all()
+    
+    def get_response_data(self, user):
+        if allauth_account_settings.EMAIL_VERIFICATION == \
+                allauth_account_settings.EmailVerificationMethod.MANDATORY:
+            return {'email': _(f'{user.email}')}
+
+        super().get_response_data(user)
