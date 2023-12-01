eba903c860f3c520d50971d5ff7bf86ea170ba02
gelono <gelonooz@gmail.com>
2023-12-01 12:29:29 +0200

    Update image tests

--- tests/fixtures/files/__init__.py
+++ tests/fixtures/files/__init__.py
@@ -1,3 +1,1 @@
-from .image import (
-    image_file
-)
+from .image import simple_image_file, compress_image_file, big_image_file, wrong_ext_image_file
--- tests/fixtures/files/image.py
+++ tests/fixtures/files/image.py
@@ -10,27 +10,70 @@ from django.core.files.base import ContentFile
 
 env = environ.FileAwareEnv()
 env.read_env(settings.BASE_DIR.joinpath(".env"))
-ROOT = env.str("ROOT", '/')
+ROOT = env.str("ROOT", "/")
 
 
-def create_image_file(image_path):
-    with open(image_path, 'rb') as image_file:
+def create_simple_image_file(image_path):
+    with open(image_path, "rb") as image_file:
         image = PilImg.open(image_file)
-        image = image.convert('RGB')
+        image = image.convert("RGB")
 
         byte_io = BytesIO()
-        image.save(byte_io, format='PNG')
+        image.save(byte_io, format="PNG")
         return ContentFile(
             byte_io.getvalue(),
-            'image.png',
+            "image.png",
         )
 
 
-def _image_path():
+def create_wrong_ext_image_file(image_path):
+    with open(image_path, "rb") as image_file:
+        image = PilImg.open(image_file)
+        image = image.convert("RGB")
+
+        byte_io = BytesIO()
+        image.save(byte_io, format="WEBP")
+        return ContentFile(
+            byte_io.getvalue(),
+            "image.webp",
+        )
+
+
+def _simple_image_path():
     from_root_path = Path("hillel-reality-i-1-i-backend/tests/fixtures/files/storage/test_image.png")
     return join(ROOT, from_root_path)
 
 
+def _compress_image_path():
+    from_root_path = Path("hillel-reality-i-1-i-backend/tests/fixtures/files/storage/test_image_751x849.png")
+    return join(ROOT, from_root_path)
+
+
+def _big_image_path():
+    from_root_path = Path("hillel-reality-i-1-i-backend/tests/fixtures/files/storage/test_image_7mb.jpg")
+    return join(ROOT, from_root_path)
+
+
+def _wrong_ext_image_path():
+    from_root_path = Path("hillel-reality-i-1-i-backend/tests/fixtures/files/storage/test_image_wrong_ext.webp")
+    return join(ROOT, from_root_path)
+
+
 @pytest.fixture()
-def image_file():
-    return create_image_file(_image_path())
+def simple_image_file():
+    return create_simple_image_file(_simple_image_path())
+
+
+@pytest.fixture()
+def compress_image_file():
+    return create_simple_image_file(_compress_image_path())
+
+
+@pytest.fixture()
+def big_image_file():
+    return create_simple_image_file(_big_image_path())
+
+
+@pytest.fixture()
+def wrong_ext_image_file():
+    return create_wrong_ext_image_file(_wrong_ext_image_path())
