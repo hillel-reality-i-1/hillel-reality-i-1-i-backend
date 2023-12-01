00b792f9cf85316ba3c6791de012f969075f20c4
Giperelk <mizerisaid1722@gmail.com>
2023-12-01 10:11:50 +0200

    Country Serializer Fix

--- apps/users/serializers/country_serializer.py
+++ apps/users/serializers/country_serializer.py
@@ -5,4 +5,4 @@ from cities_light.models import Country
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ["name"]
+        fields = ["id", "name"]
