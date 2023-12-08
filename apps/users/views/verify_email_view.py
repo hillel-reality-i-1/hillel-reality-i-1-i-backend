05596c8b283a047cd564e0dec88c0655229672ec
Giperelk <mizerisaid1722@gmail.com>
2023-12-08 16:57:15 +0200

    Fix Verify Email View

--- apps/users/views/verify_email_view.py
+++ apps/users/views/verify_email_view.py
@@ -1,18 +1,35 @@
-from dj_rest_auth.registration.views import VerifyEmailView as _VerifyEmailView
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework import status
 
+from allauth.account.views import ConfirmEmailView
 
-class VerifyEmailView(_VerifyEmailView):
+from dj_rest_auth.registration.serializers import VerifyEmailSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import MethodNotAllowed
+
+
+class VerifyEmailView(APIView, ConfirmEmailView):
+    permission_classes = (AllowAny,)
+    allowed_methods = ('POST', 'OPTIONS', 'HEAD')
+
+    def get_serializer(self, *args, **kwargs):
+        return VerifyEmailSerializer(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        raise MethodNotAllowed('GET')
+
     def post(self, request, *args, **kwargs):
-        resp = super().post(request, *args, **kwargs)
-        if resp.data == {'detail': 'ok'}:
-            email_address = self.get_object().email_address
-            user = get_user_model().objects.filter(email=email_address).first()
-            refresh = RefreshToken.for_user(user)
-            return Response(
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.kwargs['key'] = serializer.validated_data['key']
+        self.object = confirmation = self.get_object()
+        email_address = confirmation.confirm(self.request)
+        user = get_user_model().objects.filter(email=email_address).first()
+        refresh = RefreshToken.for_user(user)
+        return Response(
                 {
                     'refresh': str(refresh),
                     'access': str(refresh.access_token),
