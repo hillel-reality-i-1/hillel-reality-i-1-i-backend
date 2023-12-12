cb7362ae68909d4fe24160d20c9ef820973e8ab9
Giperelk <mizerisaid1722@gmail.com>
2023-12-12 21:18:29 +0200

    Update VerifyEmailView

--- apps/users/views/verify_email_view.py
+++ apps/users/views/verify_email_view.py
@@ -1,14 +1,15 @@
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
-from rest_framework.response import Response
-from rest_framework import status
 
 from allauth.account.views import ConfirmEmailView
 
 from dj_rest_auth.registration.serializers import VerifyEmailSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import MethodNotAllowed
+
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 
 class VerifyEmailView(APIView, ConfirmEmailView):
@@ -22,19 +23,15 @@ class VerifyEmailView(APIView, ConfirmEmailView):
         raise MethodNotAllowed('GET')
 
     def post(self, request, *args, **kwargs):
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
         self.kwargs['key'] = serializer.validated_data['key']
         self.object = confirmation = self.get_object()
+
         email_address = confirmation.confirm(self.request)
         user = get_user_model().objects.filter(email=email_address).first()
-        refresh = RefreshToken.for_user(user)
-        return Response(
-                {
-                    'refresh': str(refresh),
-                    'access': str(refresh.access_token),
-                },
-                status=status.HTTP_200_OK
-            )
+        token, created = Token.objects.get_or_create(user=user)
 
-
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
