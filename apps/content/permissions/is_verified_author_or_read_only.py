b038c6e40cdb00174cfd197bb146be1d447b712c
xsandersky <xsandersky@gmail.com>
2023-12-13 12:15:54 +0200

    change permission for create post

--- apps/content/permissions/is_verified_author_or_read_only.py
+++ apps/content/permissions/is_verified_author_or_read_only.py
@@ -1,17 +1,17 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsVerifiedAuthorOrReadOnly(BasePermission):
-
+class IsVerifiedAuthorPhoneOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        if request.method in ["GET", "HEAD", "OPTIONS"]:
             return True
 
         return obj.author == request.user
 
     def has_permission(self, request, view):
         return (
-            request.method == 'POST' and request.user.is_authenticated and request.user.is_verified()
-            or
-            request.method in ['GET', 'HEAD', 'OPTIONS']
+            request.method == "POST"
+            and request.user.is_authenticated
+            and request.user.userprofile.phone_verified
+            or request.method in ["GET", "HEAD", "OPTIONS"]
         )
