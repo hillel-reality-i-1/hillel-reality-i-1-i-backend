42c8bc0ba14ace3877d4574c30006a3dd5374fe6
gelono <gelonooz@gmail.com>
2023-12-18 09:59:31 +0200

    Fix profession endpoint

--- apps/expert/urls.py
+++ apps/expert/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views.service_view import ServiceListView, ServiceCreateView
-from .views.profession_view import ProfessionListView, ProfessionCreateView
+from .views.profession_view import ProfessionListView
+
+# from .views.profession_view import ProfessionCreateView
 
 urlpatterns = [
     path("professions/", ProfessionListView.as_view(), name="all_professions"),
-    path("professions/create", ProfessionCreateView.as_view(), name="create_profession"),
+    # path("professions/create", ProfessionCreateView.as_view(), name="create_profession"),
     #
     path("services/", ServiceListView.as_view(), name="all_services"),
     path("services/create", ServiceCreateView.as_view(), name="create_service"),
--- apps/expert/views/profession_view.py
+++ apps/expert/views/profession_view.py
@@ -1,7 +1,8 @@
-from drf_spectacular.utils import extend_schema
+# from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+
+# from rest_framework import status
 
 from ..models import Profession
 from ..serializers import ProfessionSerializer
@@ -14,20 +15,21 @@ class ProfessionListView(APIView):
         return Response(serializer.data)
 
 
-@extend_schema(request=ProfessionSerializer, responses={201: None})
-class ProfessionCreateView(APIView):
-    serializer_class = ProfessionSerializer
-
-    def post(self, request):
-        custom_profession_name = request.data.get("name")
-
-        if custom_profession_name:
-            custom_profession, created = Profession.objects.get_or_create(name=custom_profession_name)
-
-            if created:
-                serializer = ProfessionSerializer(custom_profession)
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response({"detail": "Профессия уже существует"}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({"detail": "Не указано имя профессии"}, status=status.HTTP_400_BAD_REQUEST)
+#
+# @extend_schema(request=ProfessionSerializer, responses={201: None})
+# class ProfessionCreateView(APIView):
+#     serializer_class = ProfessionSerializer
+#
+#     def post(self, request):
+#         custom_profession_name = request.data.get("name")
+#
+#         if custom_profession_name:
+#             custom_profession, created = Profession.objects.get_or_create(name=custom_profession_name)
+#
+#             if created:
+#                 serializer = ProfessionSerializer(custom_profession)
+#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             else:
+#                 return Response({"detail": "Профессия уже существует"}, status=status.HTTP_400_BAD_REQUEST)
+#
+#         return Response({"detail": "Не указано имя профессии"}, status=status.HTTP_400_BAD_REQUEST)
