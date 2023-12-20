1bead62e26019870b0588c8937ba06635592d8f2
Giperelk <mizerisaid1722@gmail.com>
2023-12-20 20:44:16 +0200

    Fix reset password email link

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
     path("createUnAccount/<str:key>/", TemplateView.as_view(), name="front_account_confirm_email"),
-    path("createNewPasswordForm/<int:user_id>/<str:key>/", TemplateView.as_view(), name="front_reset_password"),
+    path("createNewPasswordForm/<str:user_id>/<str:key>/", TemplateView.as_view(), name="front_reset_password"),
 ]
