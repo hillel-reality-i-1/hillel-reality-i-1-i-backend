cf7f4a9fc940eb89963aceb81ee0326ab40aa87a
xsandersky <xsandersky@gmail.com>
2023-12-21 21:09:43 +0200

    change api.content.urls

--- apps/content/api/urls.py
+++ apps/content/api/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
+
 from .views import DeleteUserContentView
+from apps.content.api.views.posts.post_reactions_create_view import PostReactionCreateView
+from .views.posts import PostListView, PostCreateView, PostModifiedDeleteView
+from .views.comments import CommentModifiedDeleteView, NestedCommentsCreateView, CommentsCreateView, CommentsListView
 
-from apps.content.api.views import PostViewSet, CommentViewSet
-from rest_framework.routers import DefaultRouter
-
-
-router = DefaultRouter()
-router.register('posts', PostViewSet, basename='posts')
-router.register('comments', CommentViewSet, basename='comments')
-
-app_name = 'api-content'
+app_name = "api-content"
 
 urlpatterns = [
+    path("posts/create/", PostCreateView.as_view(), name="post-create"),
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<int:pk>/", PostModifiedDeleteView.as_view(), name="post-detail"),
+    path("posts/<int:post_id>/reactions/create/", PostReactionCreateView.as_view(), name="post-reaction-create"),
+    path("posts/<int:pk>/comments/create", CommentsCreateView.as_view(), name="comment-create"),
+    path("posts/<int:pk>/comments/", CommentsListView.as_view(), name="comment-list"),
+    path("comments/<int:pk>/", CommentModifiedDeleteView.as_view(), name="comment-modified-del"),
+    path("comments/<int:pk>/replies/", NestedCommentsCreateView.as_view(), name="nested-comment-create"),
     path("delete-content/", DeleteUserContentView.as_view(), name="delete_user_content"),
 ]
-
-urlpatterns += router.urls
