0f33af4dbca79887a5b785f2eeefefd6ed8e5a49
gelono <gelonooz@gmail.com>
2023-12-21 12:39:28 +0200

    Add translation of locations

--- apps/location/management/commands/translate_city.py
+++ apps/location/management/commands/translate_city.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        df = pd.read_excel("apps/location/management/commands/cities-ukr.xlsx")
-        ukrainian_names = tuple(df["name"].tolist())
-        cities = City.objects.all().order_by("id")
+        df = pd.read_excel("apps/location/management/commands/cities-5-ukr.xlsx")
+        ukrainian_names = df["translated"].tolist()
+        cities = City.objects.all().order_by("name")
 
         for city, ukr_name in zip(cities, ukrainian_names):
             city.alternate_names = ukr_name
--- apps/location/management/commands/translate_country.py
+++ apps/location/management/commands/translate_country.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        df = pd.read_excel("apps/location/management/commands/translated_countries.xlsx")
-        ukrainian_names = tuple(df["ukrainian_names"].tolist())
-        countries = Country.objects.all().order_by("id")
+        df = pd.read_excel("apps/location/management/commands/countries-ukr-5.xlsx")
+        ukrainian_names = df["ukrainian_names"].tolist()
+        countries = Country.objects.all().order_by("name")
 
         for country, ukr_name in zip(countries, ukrainian_names):
             country.alternate_names = ukr_name
