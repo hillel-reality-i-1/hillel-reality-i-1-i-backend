405d93bb6ebd04801e8f6ce250d44d9e75847dd0
xsandersky <xsandersky@gmail.com>
2023-12-21 21:14:14 +0200

    merge

--- apps/content/api/serializers/comment_serializer.py
+++ apps/content/api/serializers/comment_serializer.py
@@ -3,6 +3,15 @@ from ...models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username", read_only=True)
+    replies = serializers.SerializerMethodField()
+
     class Meta:
         model = Comment
-        fields = ('id', 'text', 'author', 'post', 'creation_date')
+        fields = ["id", "post", "author", "text", "creation_date", "updated_date", "replies"]
+        read_only_fields = ["post"]
+
+    def get_replies(self, obj):
+        replies = Comment.objects.filter(parent=obj)
+        serializer = CommentSerializer(replies, many=True)
+        return serializer.data
--- apps/content/models/comment.py
+++ apps/content/models/comment.py
@@ -1,28 +1,18 @@
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
 from django.contrib.auth import get_user_model
 from .post import Post
-from apps.files.models import Image
 
 User = get_user_model()
 
 
 class Comment(models.Model):
-    text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
-    dislikes = models.ManyToManyField(User, related_name='comment_dislikes', blank=True)
-    images = models.ManyToManyField(Image, related_name='comment_images', blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(validators=[MinLengthValidator(2), MaxLengthValidator(10000)])
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="replies", null=True, blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Comment by {self.author}. Created at {self.creation_date}'
-
-    def total_likes(self):
-        return self.likes.count()
-
-    def total_dislikes(self):
-        return self.dislikes.count()
-
-    def get_images(self):
-        return self.images.all()
+        return f"Comment by {self.author}. Created at {self.creation_date}"
