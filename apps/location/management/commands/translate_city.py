41737d26547deda1569e9fa89f9868752b41e886
gelono <gelonooz@gmail.com>
2023-12-22 11:41:54 +0200

    Fix translation

--- apps/location/management/commands/translate_city.py
+++ apps/location/management/commands/translate_city.py
@@ -7,9 +7,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         df = pd.read_excel("apps/location/management/commands/cities-5-ukr.xlsx")
         ukrainian_names = df["translated"].tolist()
+        english_names = df["origin"].tolist()
+        dct = {e: u for e, u in zip(english_names, ukrainian_names)}
+
         cities = City.objects.all().order_by("name")
 
-        for city, ukr_name in zip(cities, ukrainian_names):
-            city.alternate_names = ukr_name
+        for city in cities:
+            city.alternate_names = dct.get(city.name)
 
         City.objects.bulk_update(cities, ["alternate_names"])
