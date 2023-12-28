1e4e5bff333bc4ce4f189de0c23ef99e5072bc82
xsandersky <xsandersky@gmail.com>
2023-12-28 12:03:44 +0200

    init files

--- apps/content/models/__init__.py
+++ apps/content/models/__init__.py
@@ -1,3 +1,3 @@
 from .post import Post
-from .comment import Comment
-from .contribution import Contribution
+from .contribution import Contribution, ContributionVote
+from .comment import Comment, UserCommentVote
