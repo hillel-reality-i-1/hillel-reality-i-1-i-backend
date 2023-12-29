e63ef86c1ab30175d8c27ea44e0d69aad1517b27
gelono <gelonooz@gmail.com>
2023-12-29 12:47:17 +0200

    Fix is_user_phone_verif.py

--- apps/content/permissions/is_user_phone_verif.py
+++ apps/content/permissions/is_user_phone_verif.py
@@ -1,8 +1,16 @@
 from rest_framework.permissions import BasePermission
 
+from apps.users.models import UserProfile
+
 
 class IsUserPhoneVerif(BasePermission):
     message = "Написання постів доступне лише користувачам з верифікованим номером телефону"
 
     def has_permission(self, request, view):
+        user = request.user
+
+        if not UserProfile.objects.filter(user=user).exists():
+            self.message = "User profile with such user does not exist"
+            return False
+
         return request.user.is_authenticated and request.user.userprofile.phone_verified
