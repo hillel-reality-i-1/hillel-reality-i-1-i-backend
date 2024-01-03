5d3f82e7c840eb051b840428b85aaa368984b992
Giperelk <mizerisaid1722@gmail.com>
2024-01-03 20:29:10 +0200

    Update profiles visualization

--- apps/users/models/user_profile_extended.py
+++ apps/users/models/user_profile_extended.py
@@ -9,4 +9,4 @@ class UserProfileExtended(models.Model):
     service = models.ForeignKey(Service, on_delete=models.PROTECT)
 
     def __str__(self):
-        return f"{self.user.username}'s extended user profile with id{self.pk}"
+        return f"{self.user.username}'s extended user profile with id {self.pk}"
