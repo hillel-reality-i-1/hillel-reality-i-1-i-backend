875981db992fe50d8fc5a9016b9a29c4e12e1c62
gelono <gelonooz@gmail.com>
2024-01-03 15:21:52 +0200

    Fix adminka with translated locations

--- apps/location/admin.py
+++ apps/location/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from apps.location.models import TranslatedCity
+from apps.location.models import TranslatedCity, TranslatedCountry
 
 admin.site.register(TranslatedCity)
+admin.site.register(TranslatedCountry)
