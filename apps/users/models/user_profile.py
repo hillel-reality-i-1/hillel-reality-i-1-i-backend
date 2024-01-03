967722090726294625f47ae9fdfd14f964179852
Giperelk <mizerisaid1722@gmail.com>
2024-01-03 20:25:43 +0200

    Update profiles visualization

--- apps/users/models/user_profile.py
+++ apps/users/models/user_profile.py
@@ -21,4 +21,4 @@ class UserProfile(models.Model):
     phone_verified_request_id = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user.username}'s user profile"
+        return f"{self.user.username}'s user profile with id{self.pk}"
