55859c72adb27fddf45b1227e2b64a1967d7f11d
xsandersky <xsandersky@gmail.com>
2024-01-08 10:35:53 +0200

    change contribution serializer

--- apps/content/api/serializers/contribution_serializer.py
+++ apps/content/api/serializers/contribution_serializer.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
 
-from apps.content.models import Contribution
+from apps.content.models import Comment
 
 
 class ContributionSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username", read_only=True)
+    helpful_count = serializers.IntegerField(read_only=True)
+    not_helpful_count = serializers.IntegerField(read_only=True)
+    is_contribution = serializers.BooleanField(read_only=True)
+
     class Meta:
-        model = Contribution
-        fields = "__all__"
-        read_only_fields = ["helpful_count"]
+        model = Comment
+        fields = [
+            "id",
+            "post",
+            "author",
+            "text",
+            "helpful_count",
+            "not_helpful_count",
+            "creation_date",
+            "updated_date",
+            "is_contribution",
+        ]
+        read_only_fields = ["post", "helpful_count", "not_helpful_count", "is_contribution"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if not instance.is_parent:
+            representation.pop("helpful_count", None)
+            representation.pop("not_helpful_count", None)
+
+        return representation
