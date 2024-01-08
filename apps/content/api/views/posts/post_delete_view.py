90c91c2eca65324f0edd42b447b900fd586afc5e
xsandersky <xsandersky@gmail.com>
2024-01-08 10:32:20 +0200

    add view

---
+++ apps/content/api/views/posts/post_delete_view.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.generics import RetrieveDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from apps.content.api.serializers import PostSerializer
+from apps.content.models import Post
+from apps.content.permissions import IsAuthorOrReadOnly
+
+
+class PostDeleteView(RetrieveDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.destroy(instance)
+        return Response({"message": "Допис успішно видалено"}, status=status.HTTP_204_NO_CONTENT)
