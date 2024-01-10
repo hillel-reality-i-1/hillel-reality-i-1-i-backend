387e00d08fe54a2fbe0877571bb775259f394b6d
Giperelk <mizerisaid1722@gmail.com>
2024-01-10 23:29:21 +0200

    Update delete user

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("createUnAccount/<str:key>/", TemplateView.as_view(), name="front_account_confirm_email"),
     path("createUnAccount/", TemplateView.as_view(), name="front_create_profile_from_social_account"),
     path("createNewPasswordForm/<str:user_id>/<str:key>/", TemplateView.as_view(), name="front_reset_password"),
-    path("deleteAllContent/<str:key>", TemplateView.as_view(), name="front_account_delete_all_content"),
+    path("deleteAllContent/<str:key>/", TemplateView.as_view(), name="front_account_delete_all_content"),
     path("set-language/<str:language_code>/", set_language_api, name="set_language"),
 ]
--- apps/users/views/user_delete_view.py
+++ apps/users/views/user_delete_view.py
@@ -1,5 +1,7 @@
+from rest_framework import status
 from rest_framework.generics import DestroyAPIView
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 
 from ..permissions import IsVerifiedUser
 from ..serializers.user_serializer import UserSerializer
@@ -15,3 +17,11 @@ class UserDeleteView(DestroyAPIView):
         obj = self.queryset.get(pk=self.request.user.id)
         self.check_object_permissions(self.request, obj)
         return obj
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            data={"details": "Your account has been successfully deleted"},
+            status=status.HTTP_204_NO_CONTENT
+        )
