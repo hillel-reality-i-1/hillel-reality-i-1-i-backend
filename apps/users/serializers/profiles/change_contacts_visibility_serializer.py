3272328baf51aebf9037bb20594638e29d7283fe
Giperelk <mizerisaid1722@gmail.com>
2024-01-15 23:28:34 +0200

    Update change visibility

--- apps/users/serializers/profiles/change_contacts_visibility_serializer.py
+++ apps/users/serializers/profiles/change_contacts_visibility_serializer.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 class ContactVisibilitySerializer(serializers.Serializer):
     contact_type = serializers.ChoiceField(
         choices=[
-            'telegram', 'instagram', 'email', 'phone', 'linkedin', 'facebook'
+            'telegram', 'instagram', 'email', 'phone_number', 'linkedin', 'facebook'
         ]
     )
