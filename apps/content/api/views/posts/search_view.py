820c73516f6c32a8f60f8d72d0610eb9c6e48371
gelono <gelonooz@gmail.com>
2024-01-15 14:18:39 +0200

    Fix search_view.py

--- apps/content/api/views/posts/search_view.py
+++ apps/content/api/views/posts/search_view.py
@@ -1,3 +1,4 @@
+from json import JSONDecodeError
 from cities_light.models import Country
 from django.contrib.postgres.search import SearchVector, SearchQuery
 from django.db.models import Q
@@ -6,6 +7,7 @@ from rest_framework import serializers
 from apps.content.api.serializers import PostSerializer
 from apps.content.models import Post
 from apps.expert.models import Category
+import json
 
 
 class SearchView(generics.ListAPIView):
@@ -17,36 +19,42 @@ class SearchView(generics.ListAPIView):
             raise serializers.ValidationError({"query": "The query parameter must be a string."})
 
         try:
-            for i in range(len(query_data["countries_id"])):
-                query_data["countries_id"][i] = int(query_data["countries_id"][i])
-        except Exception:
-            raise serializers.ValidationError({"country_id": "The country_id parameter must be a list of integers."})
+            if query_data["countries_id"]:
+                query_data["countries_id"] = list(json.loads(query_data["countries_id"]))
+        except (TypeError, JSONDecodeError):
+            raise serializers.ValidationError({"country_id": "The country_ids parameter must be a list of integers."})
 
         try:
-            for i in range(len(query_data["categories_id"])):
-                query_data["categories_id"][i] = int(query_data["categories_id"][i])
-        except Exception:
-            raise serializers.ValidationError({"category_id": "The category_id parameter must be a list of integers."})
+            if query_data["categories_id"]:
+                query_data["categories_id"] = list(json.loads(query_data["categories_id"]))
+        except (TypeError, JSONDecodeError):
+            raise serializers.ValidationError({"category_id": "The category_ids parameter must be a list of integers."})
+
+        return query_data
+
+    @staticmethod
+    def get_values(query_data, model, key: str):
+        values = list(model.objects.filter(id__in=query_data.get(key)).values_list("name", flat=True))
+        if not values:
+            raise serializers.ValidationError({f"{key}": f"The specified {key} do not exist in the database."})
+
+        return values
 
     def get_queryset(self):
         query = self.request.GET.get("query", "")
-        countries_id = self.request.GET.getlist("country_id", [])
-        categories_id = self.request.GET.getlist("category_id", [])
+        countries_id = self.request.GET.get("country_ids", [])
+        categories_id = self.request.GET.get("category_ids", [])
 
-        self.validate_query_data({"query": query, "countries_id": countries_id, "categories_id": categories_id})
+        query_data = self.validate_query_data(
+            {"query": query, "countries_id": countries_id, "categories_id": categories_id}
+        )
 
         # Checking the availability of search data
         if not query and not countries_id and not categories_id:
             return []
 
-        if countries_id:
-            countries = list(Country.objects.filter(id__in=countries_id).values_list("name", flat=True))
-        else:
-            countries = None
-        if categories_id:
-            categories = list(Category.objects.filter(id__in=categories_id).values_list("name", flat=True))
-        else:
-            categories = None
+        countries = self.get_values(query_data, Country, "countries_id") if countries_id else None
+        categories = self.get_values(query_data, Category, "categories_id") if categories_id else None
 
         # Search filter for Post
         post_queryset = (
