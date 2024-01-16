286b712076ba44ed04b5f5d2cb3fbd7bee394fdb
gelono <gelonooz@gmail.com>
2024-01-16 12:55:42 +0200

    Add deleting expert profile with user profile

--- apps/users/views/user_profile_view.py
+++ apps/users/views/user_profile_view.py
@@ -1,15 +1,10 @@
-# from rest_framework.permissions import IsAdminUser
-# from rest_framework import viewsets
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import mixins
-
-# from rest_framework.views import APIView
-from apps.users.models import UserProfile
+from apps.users.models import UserProfile, UserProfileExtended
 from apps.users.permissions import IsAdminOrProfileOwner
-
-# from rest_framework.response import Response
 from apps.users.serializers.user_profile_serializer import UserProfileSerializer
 
 
@@ -20,9 +15,6 @@ class UserProfileListView(
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated, IsAdminOrProfileOwner]
 
-    # def get_object(self):
-    #     return self.request.user.userprofile
-
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=True)
@@ -30,3 +22,15 @@ class UserProfileListView(
         self.perform_update(serializer)
 
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        try:
+            user_profile_extended = UserProfileExtended.objects.get(user=instance.user)
+            user_profile_extended.delete()
+        except UserProfileExtended.DoesNotExist:
+            pass
+        self.perform_destroy(instance)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
