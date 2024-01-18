ad0af9dc77bc690dc22aa29678acbd5d85f1b661
gelono <gelonooz@gmail.com>
2024-01-18 17:44:41 +0200

    Fix urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,8 @@
 from apps.files.api.views.upload_portfolio_view import UploadPortfolioView
-
-# from django.views.generic import TemplateView
-
 from apps.users.views.change_email_request_view import ChangeEmailRequestView, ChangeEmailConfirmView
 from apps.users.views.custom_google_login_view import SocialLoginView
 from apps.users.views.check_user_with_email_exist_view import CheckEmailExists
-from apps.users.views.reset_password.password_reset_custom_view import PasswordResetView
+from apps.users.views.custom_password_reset_view import PasswordResetView
 from apps.users.views.custom_register_view import CustomRegisterView
 from dj_rest_auth.registration.views import (
     ResendEmailVerificationView,
@@ -14,9 +11,9 @@ from django.urls import path, include
 
 from apps.users.views.delete_all_content_confirm_view import DeleteAllContentConfirmView
 from apps.users.views.delete_all_content_view import DeleteAllContentView
+from apps.users.views.password_reset_confirm_view import PasswordResetConfirmView
 from apps.users.views.profiles.get_expert_user_profile_by_user_id_view import ExpertUserProfileByUserIdView
 from apps.users.views.profiles.get_user_profile_by_user_id_view import UserProfileByUserIdView
-from apps.users.views.reset_password.password_reset_confirm_view import PasswordResetConfirmView
 from apps.users.views.profiles.change_visibility import ChangeContactVisibilityView
 from apps.users.views.profiles.open_user_info_view import UserOpenInfoView
 from apps.users.views.profiles.register_user_profile_ext_view import RegisterProfileExtView
@@ -101,7 +98,7 @@ change_email_urlpatterns = [
     path(
         "change-email/confirm/<str:uidb64>/<str:token>/<str:new_email>",
         ChangeEmailConfirmView.as_view(),
-        name="change-email-confirm"
+        name="change-email-confirm",
     ),
 ]
 
