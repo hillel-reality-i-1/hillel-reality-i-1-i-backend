e06c4145756ba76fa0c20d656ec4a90bfb48a92c
gelono <gelonooz@gmail.com>
2024-01-25 16:42:25 +0200

    Fix portfolio_list_view.py, user_profile_extended_view.py

--- apps/files/api/views/portfolio_list_view.py
+++ apps/files/api/views/portfolio_list_view.py
@@ -1,11 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+
 from apps.files.api.serializers.porfolio_serializer import PortfolioSerializer
 from apps.files.models import File
 from apps.users.permissions import IsAdminOrPortfolioOwner
 
 
-class PortfolioListView(viewsets.ModelViewSet):
+class PortfolioListView(
+    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, GenericViewSet
+):
     queryset = File.objects.all()
     serializer_class = PortfolioSerializer
     permission_classes = [IsAuthenticated, IsAdminOrPortfolioOwner]
--- apps/users/views/profiles/user_profile_extended_view.py
+++ apps/users/views/profiles/user_profile_extended_view.py
@@ -1,11 +1,14 @@
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
+from rest_framework.viewsets import GenericViewSet
 from apps.users.models import UserProfileExtended
 from apps.users.permissions import IsAdminOrProfileExtendedOwner
 from apps.users.serializers.user_profile_extended_serializer import UserProfileExtendedSerializer
 
 
-class UserProfileExtendedListView(viewsets.ModelViewSet):
+class UserProfileExtendedListView(
+    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, GenericViewSet
+):
     queryset = UserProfileExtended.objects.all()
     serializer_class = UserProfileExtendedSerializer
     permission_classes = [IsAuthenticated, IsAdminOrProfileExtendedOwner]
