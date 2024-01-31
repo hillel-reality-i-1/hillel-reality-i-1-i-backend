021730b5b3aae63ce49e992e3c7e0334568951df
gelono <gelonooz@gmail.com>
2024-01-31 18:36:44 +0200

    Fix user_profile.py

--- apps/users/models/user_profile.py
+++ apps/users/models/user_profile.py
@@ -3,10 +3,14 @@ import re
 from cities_light.models import Country, City
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from rest_framework.exceptions import ValidationError
+
 from apps.content.models import Post, Comment
 from apps.files.models import Image
 from apps.users.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.phonenumber import PhoneNumber
+from phonenumber_field.validators import validate_international_phonenumber
 from rest_framework import serializers
 
 
@@ -73,54 +77,76 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s user profile with id {self.pk}"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # Call the parent's __init__ method
+        self.set_origins_fields()
+
+    def set_origins_fields(self):
+        self._original_telegram = self.telegram
+        self._original_instagram = self.instagram
+        self._original_facebook = self.facebook
+        self._original_linkedin = self.linkedin
+        self._original_phone_number = self.phone_number
+
     def clean(self):
         # Check unique of telegram, instagram, facebook, linkedin
-        if (
-            self.telegram is not None
-            and UserProfile.objects.exclude(pk=self.pk).filter(telegram=self.telegram).exists()
-        ):
-            raise serializers.ValidationError({"telegram": "Such telegram account already exists."})
-        else:
-            self.validate_telegram(self.telegram)
+        if self.telegram != self._original_telegram:
+            if (
+                self.telegram is not None
+                and UserProfile.objects.exclude(pk=self.pk).filter(telegram=self.telegram).exists()
+            ):
+                raise serializers.ValidationError({"telegram": "Such telegram account already exists."})
+            else:
+                self.validate_telegram(self.telegram)
 
-        if (
-            self.instagram is not None
-            and UserProfile.objects.exclude(pk=self.pk).filter(instagram=self.instagram).exists()
-        ):
-            raise serializers.ValidationError({"instagram": "Such instagram account already exists."})
-        else:
-            self.validate_instagram(self.instagram)
+        if self.instagram != self._original_instagram:
+            if (
+                self.instagram is not None
+                and UserProfile.objects.exclude(pk=self.pk).filter(instagram=self.instagram).exists()
+            ):
+                raise serializers.ValidationError({"instagram": "Such instagram account already exists."})
+            else:
+                self.validate_instagram(self.instagram)
 
-        if (
-            self.facebook is not None
-            and UserProfile.objects.exclude(pk=self.pk).filter(facebook=self.facebook).exists()
-        ):
-            raise serializers.ValidationError({"facebook": "Such facebook account already exists."})
-        else:
-            self.validate_facebook(self.facebook)
+        if self.facebook != self._original_facebook:
+            if (
+                self.facebook is not None
+                and UserProfile.objects.exclude(pk=self.pk).filter(facebook=self.facebook).exists()
+            ):
+                raise serializers.ValidationError({"facebook": "Such facebook account already exists."})
+            else:
+                self.validate_facebook(self.facebook)
 
-        if (
-            self.linkedin is not None
-            and UserProfile.objects.exclude(pk=self.pk).filter(linkedin=self.linkedin).exists()
-        ):
-            raise serializers.ValidationError({"linkedin": "Such linkedin account already exists."})
-        else:
-            self.validate_linkedin(self.linkedin)
+        if self.linkedin != self._original_linkedin:
+            if (
+                self.linkedin is not None
+                and UserProfile.objects.exclude(pk=self.pk).filter(linkedin=self.linkedin).exists()
+            ):
+                raise serializers.ValidationError({"linkedin": "Such linkedin account already exists."})
+            else:
+                self.validate_linkedin(self.linkedin)
 
-        if (
-            self.phone_number is not None
-            and UserProfile.objects.exclude(pk=self.pk)
-            .filter(phone_number=self.phone_number, phone_verified=True)
-            .exists()
-        ):
-            raise serializers.ValidationError({"phone_number": "Such phone_number already verified."})
-        else:
-            self.validate_linkedin(self.linkedin)
+        if self.phone_number != self._original_phone_number:
+            if (
+                self.phone_number is not None
+                and UserProfile.objects.exclude(pk=self.pk)
+                .filter(phone_number=self.phone_number, phone_verified=True)
+                .exists()
+            ):
+                raise serializers.ValidationError({"phone_number": "Such phone_number already verified."})
+            else:
+                try:
+                    if self.phone_number:
+                        phone_instance = PhoneNumber.from_string(str(self.phone_number))
+                        validate_international_phonenumber(phone_instance)
+                except ValidationError as e:
+                    raise serializers.ValidationError({"phone_number": str(e)})
 
         super().clean()
 
     def save(self, *args, **kwargs):
         self.clean()
+        self.set_origins_fields()
         super().save(*args, **kwargs)
 
     @staticmethod
