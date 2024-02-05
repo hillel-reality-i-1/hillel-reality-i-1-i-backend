e4f19a90dac748fd80148804be8330d9719fe853
xsandersky <xsandersky@gmail.com>
2024-02-05 10:56:11 +0200

    add paginations to comment/contrib

--- apps/content/api/views/contribution_view.py
+++ apps/content/api/views/contribution_view.py
@@ -2,12 +2,14 @@ from rest_framework.generics import ListAPIView
 
 from apps.content.api.serializers.contribution_serializer import ContributionSerializer
 from apps.content.models import Comment
+from apps.users.views.last_activity.last_activity_pagination import LastFiveActivityPagination
 
 
 class ContributionListView(ListAPIView):
     serializer_class = ContributionSerializer
+    pagination_class = LastFiveActivityPagination
 
     def get_queryset(self):
-        post_id = self.kwargs.get("post_id")
+        post_id = self.kwargs["pk"]
         queryset = Comment.objects.filter(post_id=post_id, is_contribution=True).order_by("-creation_date")
         return queryset
